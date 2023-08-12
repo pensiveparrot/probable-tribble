2aafd98a22f44b3c0cf95ad9c0a0e9f5edd8a250
kzxking <unidlinghost@gmail.com>
2023-08-11 21:07:26 -0400

    fix visual bug

--- test/src/app/shop/shop.component.ts
+++ test/src/app/shop/shop.component.ts
@@ -15,7 +15,7 @@ export class ShopComponent {
       this.showText=true;
     }, 4000);
   }
-products: Product[] = [ { name: 'Shirt', price: 30, inventoryStatus:'In Stock', image: 'assets/products/1.jpg',productLink:'https://buy.stripe.com/test_8wM5mQcoKcxhdHOcMM' }, { name: 'Jeans', price: 50, inventoryStatus:'Out of Stock', image: '../assets/products/2.jpg',productLink:'' }, { name: 'Sneakers', price: 60, inventoryStatus:'Low Stock', image: '../assets/products/3.jpg',productLink:'' }];
+products: Product[] = [ { name: 'Shirt', price: 30, inventoryStatus:'In Stock', image: 'assets/products/1.jpg',productLink:'https://buy.stripe.com/test_8wM5mQcoKcxhdHOcMM' }, { name: 'Jeans', price: 50, inventoryStatus:'Out Of Stock', image: '../assets/products/2.jpg',productLink:'' }, { name: 'Sneakers', price: 60, inventoryStatus:'Low Stock', image: '../assets/products/3.jpg',productLink:'' }];
 
 
 responsiveOptions: any[] = [ { breakpoint: '1024px', numVisible: 3, numScroll: 3 }, { breakpoint: '768px', numVisible: 2, numScroll: 2 }, { breakpoint: '560px', numVisible: 1, numScroll: 1 } ];
